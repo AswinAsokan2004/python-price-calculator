4bbe4013b8ac9aff5dcc31b5b9ba89511634822e
AswinAsokan2004 <aswinasokan200426@gmail.com>
2026-05-19 16:27:20 +0530

    1

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def calculate_total(price, quantity, percentageDiscount):
     total = price * quantity
-    total -= total * percentageDiscount / 100
+    total *= total * percentageDiscount / 100
     return total
 
 def main():
